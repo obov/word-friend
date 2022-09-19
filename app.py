e1d6c3796a023dca8e33e038bf385a8db112a5f6
obov <obov3e@gmail.com>
2022-09-19 13:53:20 +0900

    anchers linked

--- app.py
+++ app.py
@@ -5,6 +5,21 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+@app.route('/word/enlist')
+def enlist():
+    return render_template('/word/enlist.html')
+@app.route('/word/exam')
+def exam():
+    return render_template('/word/exam.html')
+@app.route('/word/enlisted')
+def enlisted():
+    return render_template('/word/enlisted.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
