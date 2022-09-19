ac5aa3a165a4fcef3c97e3a1cb5236a7c0ce7ab9
kwon-ga <rnjsdydry@gmail.com>
2022-09-19 23:23:53 +0900

    검색기능 임시추가

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from mimetypes import init
 from types import MethodType
 from flask import Flask ,render_template, session,jsonify,request
 import requests
 from pymongo import MongoClient
+from bs4 import BeautifulSoup
 
 
 #db 접속
@@ -34,6 +36,68 @@ def exam():
 def enlisted():
     return render_template('/word/enlisted.html')
 
+
+#자동완성
+@app.route("/search_word",methods=["post"])
+def search_word():
+
+    keyword = request.form['keyword']
+    #크롤링
+    #https://dic.daum.net/search.do?q=apple&dic=eng&search_first=Y
+    if keyword.isalpha():
+        try:    
+            doc=[]
+            data = requests.get("https://dic.daum.net/search.do?q="+keyword+"&dic=eng&search_first=Y", headers=headers)
+            soup = BeautifulSoup(data.text, 'html.parser')
+
+            #정확한 단어
+            cleanword = soup.find('a',{'class','txt_cleansch'}).text
+            clean_href = soup.find('a',{'class','txt_cleansch'})['href']
+            clean_intend = soup.find('ul',{'class','list_search'}).text
+            # print(cleanword,clean_intend,clean_href)
+            doc.append({
+                'word' : cleanword,
+                'href' : clean_href,
+                'intend' : clean_intend
+            })
+            #연관 검색어
+            data2 = soup.select('#mArticle > div.search_cont > div:nth-child(3) > div:nth-child(3) > div')
+            
+            for words in data2:
+                word = words.select_one('strong > a').text
+                intend = words.select_one('ul').text
+                href = words.select_one('strong > a')['href']
+
+                doc.append({
+                    'word' : word,
+                    'intend' : intend,
+                    'href' : href
+                })
+
+            return jsonify({'data':doc})
+        except:
+            return jsonify({'data':'검색 결과가 없습니다.'})
+
+    else:   
+        try: 
+            data = requests.get("https://dic.daum.net/search.do?q="+keyword+"&dic=eng", headers=headers)
+            soup = BeautifulSoup(data.text, 'html.parser')
+            data = soup.select('#mArticle > div.search_cont > div:nth-child(4) > div.search_box > div')
+            doc=[]
+            for search in data:
+                word = search.select_one('div.search_word > strong > a').text
+                href = search.select_one('div.search_word > strong > a')['href']
+                intend = search.select_one('ul').text
+                print(word,href,intend)
+                doc.append({
+                    'word':word,
+                    'href':href,
+                    'intend':intend
+                })
+            return jsonify({'data':doc})
+        except:   
+            return jsonify({'data':'검색 결과가 없습니다.'})
+
 #로그인 api (session)
 @app.route("/login",methods=["post"])
 def log_in():
@@ -56,13 +120,13 @@ def log_in():
     except:        
         return jsonify({'msg': '일치하는 계정이 없습니다.','result':'false'})
 
-
+#로그아웃
 @app.route("/logout",methods=["post"])
 def log_out():
     session.pop('id',None)
     return jsonify({'result':'성공!!'})
 
-
+#로그인체크
 @app.route("/login_check",methods=["post"])
 def  login_check():
     if "id" in session:
@@ -70,7 +134,15 @@ def  login_check():
     else :
         return jsonify({'loginData': 'notlogin'})
 
+#로그인체크
+@app.route("/word/login_check",methods=["post"])
+def  word_login_check():
+    if "id" in session:
+        return jsonify({'loginData': session['id']})
+    else :
+        return jsonify({'loginData': 'notlogin'})
 
+#회원가입
 @app.route("/sign_up",methods=["post"])
 def  sign_up():
     try:
