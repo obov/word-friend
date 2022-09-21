e626dedce1552345c92595a246a5f4aa381f3e33
obov <obov3e@gmail.com>
2022-09-21 15:38:56 +0900

    titles // card slider

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,19 +1,22 @@
 // Home animations
-const wordSlider = function () {
+const wordSlider = function (part) {
+  const words = () => $(`#${part} > .words`);
+  const showingCard = () => $(`#${part}ShowingCard`);
+  const cards = () => $(`#${part} > .words .cards`);
   const handleClickShowing = function () {
     const clickTimeEnd = new Date().getTime();
-    if (clickTimeEnd - clickTime < 100 && !isMoving) {
-      showWordChecker($("#showing-card").text(), true, false);
+    if (clickTimeEnd - clickTime < 100 && !isMovingNow) {
+      showWordChecker(showingCard().text(), true, false);
     }
   };
 
   let dragDist = 0;
   let isMovable = true;
-  let isMoving = false;
+  let isMovingNow = false;
   let clickTime = 0;
   let showIndex = 0;
 
-  const values = $("#words").data("values").split("-");
+  const values = words().data("values").split("-");
   const cycleFromArr = (arr) => (number) => {
     const { length: len } = arr;
     return ((number % len) + len) % len;
@@ -21,15 +24,16 @@ const wordSlider = function () {
   const cycle = cycleFromArr(values);
   const datasToShow = (showIndex) => [-1, 0, 1].map((e) => values[cycle(e + showIndex)]);
   const insertValuesOnCards = function () {
-    $(".cards").each(function (index, card) {
+    cards().each(function (index, card) {
       $(card).text(datasToShow(showIndex)[index]);
     });
   };
 
-  $("#showing-card").on("touchstart mousedown", function () {
+  showingCard().on("touchstart mousedown", function () {
     clickTime = new Date().getTime();
   });
-  $("#showing-card").on("touchend mouseup", handleClickShowing());
+
+  showingCard().on("touchend mouseup", handleClickShowing);
 
   insertValuesOnCards();
 
@@ -41,7 +45,7 @@ const wordSlider = function () {
     } else {
       startPoint = e.pageX;
     }
-    $("#words").on("touchmove mousemove", dragMove);
+    words().on("touchmove mousemove", dragMove);
   };
 
   function dragMove(e) {
@@ -50,32 +54,32 @@ const wordSlider = function () {
     } else {
       movePoint = e.pageX;
     }
-    isMoving = true;
+    isMovingNow = true;
     if (isMovable) {
       dragDist = ((movePoint - startPoint) / $("#favorite").width()) * 100;
       if (dragDist < 0) {
-        $("#words").css("left", dragDist - 100 + "%");
+        words().css("left", dragDist - 100 + "%");
         if (dragDist < -20) {
           isMovable = false;
-          $("#words").css("transition", "all 0.3s");
-          $("#words").css("left", "-200%");
+          words().css("transition", "all 0.3s");
+          words().css("left", "-200%");
           setTimeout(function () {
-            $("#words").css("transition", "none");
-            $("#words").css("left", "-100%");
+            words().css("transition", "none");
+            words().css("left", "-100%");
             showIndex = cycle(showIndex + 1);
             insertValuesOnCards();
           }, 300);
         }
       }
       if (dragDist > 0) {
-        $("#words").css("left", dragDist - 100 + "%");
+        words().css("left", dragDist - 100 + "%");
         if (dragDist > 20) {
           isMovable = false;
-          $("#words").css("transition", "all 0.3s");
-          $("#words").css("left", "0");
+          words().css("transition", "all 0.3s");
+          words().css("left", "0");
           setTimeout(function () {
-            $("#words").css("transition", "none");
-            $("#words").css("left", "-100%");
+            words().css("transition", "none");
+            words().css("left", "-100%");
             showIndex = cycle(showIndex - 1);
             insertValuesOnCards();
           }, 300);
@@ -85,30 +89,33 @@ const wordSlider = function () {
   }
   const dragEnd = function (e) {
     e.preventDefault();
-    $("#words").off("touchmove mousemove", dragMove);
+    words().off("touchmove mousemove", dragMove);
+    isMovingNow = false;
     if (Math.abs(dragDist) < 20) {
-      $("#words").css("transition", "all 0.3s");
-      $("#words").css("left", "-100%");
+      words().css("transition", "all 0.3s");
+      words().css("left", "-100%");
       setTimeout(function () {
-        $("#words").css("transition", "none");
+        words().css("transition", "none");
       }, 300);
     } else {
       isMovable = true;
-      isMoving = false;
     }
     dragDist = 0;
   };
 
-  $("#words").on("touchstart mousedown", dragStart);
-  $("#words").on("touchend mouseup", dragEnd);
+  words().on("touchstart mousedown", dragStart);
+  words().on("touchend mouseup", dragEnd);
 };
 const whenHomeOpened = function () {
-  wordSlider();
+  wordSlider("favorite");
+  wordSlider("recent");
+  wordSlider("recap");
 };
+
+// Auth animations
 const hideHamburger = function () {
   $(".menu-toggle").hide();
 };
-// Auth animations
 const whenAuthOpened = function () {
   hideHamburger();
 };
